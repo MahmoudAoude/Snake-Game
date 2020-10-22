8d559a9bff6cebe7b6cd2c6e06577fde6e824488
mma37 <mma37@hw.ac.uk>
2020-10-22 14:09:50 +0400

    Fixed a minor bug.

--- src/server/game/snake.js
+++ src/server/game/snake.js
@@ -1,4 +1,4 @@
-import {collidedWithSelf} from './collisionWithSelf.js'
+import {collidedWithSelf} from './collision.js'
 
 export default class Snake {
 
@@ -25,24 +25,24 @@ export default class Snake {
     updateTail(n) { // extends tail of snake by n
         const x = 0
         const y = 1
-        const previousTailPosition = this.body[this.tailIndex]
+        let previousTailPosition = this.body[this.tailIndex]
 
-        console.log(this.body)
+        // console.log(this.body)
       
         for (var i = 0; i < this.body.length; i++) {
             console.log(this.body[i])
         }
-        console.log("update tail:")
-        console.log(n)
-        console.log(" UP PART")
-        console.log(this.tailIndex)
-        console.log(this.body[this.tailIndex][y])
-        console.log(this.body[this.tailIndex][x])
+        // console.log("update tail:")
+        // console.log(n)
+        // console.log(" UP PART")
+        // console.log(this.tailIndex)
+        // console.log(this.body[this.tailIndex][y])
+        // console.log(this.body[this.tailIndex][x])
         this.setLastTailLocation(this.body[this.tailIndex][x], this.body[this.tailIndex][y]);
 
         this.tailIndex += n;
-        console.log("Tail extended by",n)
-        console.log("New tail location:",this.body[this.tailIndex])
+        // console.log("Tail extended by",n)
+        // console.log("New tail location:",this.body[this.tailIndex])
     }
     setLastTailLocation(x, y) {
         console.log("Last tail location was [",x,",",y,"]")
@@ -55,20 +55,20 @@ export default class Snake {
         this.body.push([x,y]);
         this.updateTail(1);
 
-        console.log("Now the snake is",this.body.length,"parts long")
+        // console.log("Now the snake is",this.body.length,"parts long")
 
     }
 
     move(direction) {
         const x = 0
         const y = 1
-        console.log("Now moving...")
-        console.log("Tail position:",this.tailIndex,"parts from head")
-        console.log(this.tailIndex)
-        console.log(this.body[this.tailIndex-1][y])
-        console.log(this.body[this.tailIndex-1][x])
+        // console.log("Now moving...")
+        // console.log("Tail position:",this.tailIndex,"parts from head")
+        // console.log(this.tailIndex)
+        // console.log(this.body[this.tailIndex-1][y])
+        // console.log(this.body[this.tailIndex-1][x])
         
-        this.setLastTailLocation(this.body[this.tailIndex-1][x], this.body[this.tailIndex-1][y])
+        //this.setLastTailLocation(this.body[this.tailIndex][x], this.body[this.tailIndex][y]) //removed fOR TESTING (Useless)
 
         for (var i = this.tailIndex - 1; i >= 0; i--) { // from tail, to 1 (not 0 or head)
             //console.log("Inside for loop");
@@ -98,14 +98,14 @@ export default class Snake {
                 break;
         }
         this.setdirectionHeading(direction);
-        console.log("Snake's head is at",this.body[0])
+        // console.log("Snake's head is at",this.body[0])
     }
 
     increaseLength(n) { // increases length of snake by n by adding blocks to it's tail. NOT YET IMPLEMENTED NEGATIVE VALUES FOR n
         for (var i = 0; i < n; i++) {
             // adds element to Snake making it longer by 1, by pushing the last position the tail of the snake was on
-            this.body.push(this.body.lastTailLocation);
             this.updateTail(1);
+            this.body.push(this.lastTailLocation);
         }
     }
     ateFood(n) { // player at food, so increase length of snake by n. NOT YET IMPLEMENTED NEGATIVE VALUES FOR n
